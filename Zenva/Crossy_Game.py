4be75dd50090efc90cb644ccae0e8504dc2a85dc
Guiwald <gwxtrash@gmail.com>
2018-12-25 18:22:29 +0000

    updating game

--- Zenva/Crossy_Game.py
+++ Zenva/Crossy_Game.py
@@ -5,7 +5,8 @@ pygame.init()
 
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 800
-WHITE_COLOR = (255, 255, 255)
+SCREEN_TITLE = 'Crossy Game'
+WHITE_COLOR = (100, 255, 255)
 BLACK_COLOR = (0, 0, 0)
 clock = pygame.time.Clock()
 TICK_RATE = 60
@@ -14,12 +15,14 @@ is_game_over = False
 
 game_screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 game_screen.fill(WHITE_COLOR)
+pygame.display.set_caption(SCREEN_TITLE)
 
 while not is_game_over:
 
 	for event in pygame.event.get():
-		if event.type = pygame.QUIT:
+		if event.type == pygame.QUIT:
 			is_game_over = True
+		print(event)
 
 	pygame.display.update()
 	clock.tick(TICK_RATE)
